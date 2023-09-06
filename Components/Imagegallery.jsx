5a7e5696bafe2cba642decb983ecb8e857af8126
lee14k <kmkovach@mtu.edu>
2023-09-06 12:09:16 -0400

    working on image gallery api routing

--- Components/Imagegallery.jsx
+++ Components/Imagegallery.jsx
@@ -1,47 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function Imagegallery({ categorizedMedia }) {
-  console.log(mediaItems);
 
+function Imagegallery() {
   const [isModalOpen, setModalOpen] = useState(false);
-  const [currentImage, setCurrentImage] = useState("");
-  const [currentCategory, setCurrentCategory] = useState("Category1"); // default category
+  const [currentImage, setCurrentImage] = useState('');
+  const [categorizedMedia, setCategorizedMedia] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('/api/images');
+      setCategorizedMedia(response.data);
+    };
+
+    fetchData();
+  }, []);
 
   const handleOpenModal = (image) => {
-    setCurrentImage(image.source_url); // Use source_url, which is the URL of the media item in WordPress
+    setCurrentImage(image);
     setModalOpen(true);
   };
 
   const handleCloseModal = () => {
-    setCurrentImage("");
+    setCurrentImage('');
     setModalOpen(false);
   };
 
   return (
     <div className="gallerycontainer">
+      {/* The rest of your component stays the same. Replace `categorizedMedia` with the state variable. */}
       <div className="container">
-        {/* Category Tabs */}
+        {/* Create Tabs */}
         <div className="tabs">
-          {Object.keys(categorizedMedia).map(category => (
-            <button onClick={() => setCurrentCategory(category)}>
-              {category}
-            </button>
+          {Object.keys(categorizedMedia).map((folderName) => (
+            <button key={folderName}>{folderName}</button>
           ))}
         </div>
+        
+        {/* Create Image Gallery under each tab */}
+        {Object.keys(categorizedMedia).map((folderName) => (
+          <div key={folderName} className="gallery">
+            {categorizedMedia[folderName].map((item) => (
+              <figure key={item.id} className={`gallery__item gallery__item--${item.id}`}>
+                <img
+                  src={item.source_url} // make sure this is the correct field
+                  alt={item.alt_text} // make sure this is the correct field
+                  className="gallery__img"
+                  onClick={() => handleOpenModal(item.source_url)}
+                />
+              </figure>
+            ))}
+          </div>
+        ))}
 
-        {/* Gallery */}
-        <div className="gallery">
-          {categorizedMedia[currentCategory].map((item, index) => (
-            <figure className={`gallery__item gallery__item--${index + 1}`} key={item.id}>
-              <img
-                src={item.source_url}
-                alt={item.alt_text}
-                className="gallery__img"
-                onClick={() => handleOpenModal(item)}
-              />
-            </figure>
-          ))}
-        </div>
       </div>
       {isModalOpen && (
         <div className="modal">
---
+++ pages/api/images.js
@@ -0,0 +1,22 @@
+// pages/api/media.js
+import axios from 'axios';
+
+export default async function handler(req, res) {
+  try {
+    const response = await axios.get('https://kaileehamre.com/wp-json/wp/v2/media');
+    const mediaItems = response.data;
+
+    const categorizedMedia = mediaItems.reduce((acc, item) => {
+      const folderName = item.rml_folder_info?.name || 'Uncategorized';
+      if (!acc[folderName]) {
+        acc[folderName] = [];
+      }
+      acc[folderName].push(item);
+      return acc;
+    }, {});
+
+    res.status(200).json(categorizedMedia);
+  } catch (error) {
+    res.status(500).json({ error: 'Unable to fetch data' });
+  }
+}
