8d959b5230f59f9517d87443892979d920ffc6f1
lee14k <kmkovach@mtu.edu>
2023-09-30 09:05:07 -0400

    messing with event cards

--- Components/Eventspagevent.jsx
+++ Components/Eventspagevent.jsx
@@ -1,28 +1,43 @@
-export default function Eventspageevent({ date, time, header, description, price }) {
-  
+export default function Eventspageevent({
+  date,
+  time,
+  header,
+  description,
+  price,
+  flipped,
+  onClick
+}) {
   let bgImage;
 
   if (header.toLowerCase().includes("membership")) {
-    bgImage = 'url(/7.jpg)';
+    bgImage = "url(/7.jpg)";
   } else if (header.toLowerCase().includes("board")) {
-    bgImage = 'url(/four.jpg)';
+    bgImage = "url(/four.jpg)";
   } else {
-    bgImage = 'url(/midwinepic.jpeg)';
+    bgImage = "url(/midwinepic.jpeg)";
   }
 
   return (
-    <div style={{ backgroundImage: bgImage }}>
-      <div className="grid gap-10 items-start mt-20">
-        <div>
-          <div>{date}</div>
-          <div>{time}</div>
+    <div className={`flip-card ${flipped ? "flipped" : ""}`} onClick={onClick}>
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+          <div className="eventContainer" style={{ backgroundImage: bgImage }}>
+            <div className="grid gap-10 items-start mt-20">
+              <div>
+                <div>{date}</div>
+                <div>{time}</div>
+              </div>
+              <div>
+                <h3>{header}</h3>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="">
-          <h3>{header}</h3>
-          <p>{description}</p>
-        </div>
-        <div>
-          {price && <span>{price}</span>}
+        <div className="flip-card-back">
+          <div className="back-content">
+            <p>{description}</p>
+            {price && <span>{price}</span>}
+          </div>
         </div>
       </div>
     </div>
