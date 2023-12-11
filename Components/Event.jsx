1e4b68ea9727f19c6809f966fff43c9b87f37171
lee14k <kmkovach@mtu.edu>
2023-12-11 12:00:37 -0500

    formatting on home page events

--- Components/Event.jsx
+++ Components/Event.jsx
@@ -1,18 +1,21 @@
 export default function Event({ date, time, header, description, price }) {
   return (
     <div className="">
-    <div className="grid grid-cols-3 gap-4 items-start mt-20">
+    <div className="grid grid-cols-3 gap-24 items-start mt-20">
       <div>
    
       </div>
       <div className="">
         <div className="flex justify-center items-center homeeventhead text-xl">
+          <div className="flex flex-col justify-center items-center">
             <div>{date}</div>
         <div>{time}</div>
         </div>
-        <h3 className="homeeventhead text-4xl flex justify-center items-center">{header}</h3>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+        <h3 className="homeeventhead text-4xl flex flex-col justify-center items-center">{header}</h3>
         <p className="text-xl">{description}</p>
-         
+         </div>
       </div>
       <div>
         {price && <span>{price}</span>}
