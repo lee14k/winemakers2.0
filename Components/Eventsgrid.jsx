25bc4733df240751ad3d4abb17ca8e28e5340cee
lee14k <kmkovach@mtu.edu>
2025-03-16 12:06:18 -0500

    trying to fix link padding

--- Components/Eventsgrid.jsx
+++ Components/Eventsgrid.jsx
@@ -49,6 +49,7 @@ export default function Eventsgrid({ events }) {
                 target="_blank"
                 rel="noopener noreferrer"
                 className="event-link-button"
+                onClick={(e) => e.stopPropagation()}
               >
                 <button className="block w-full rounded-md bg-red-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">
                   More Info
