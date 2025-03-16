e66987e26a9f83ea2e7798a0de4bbebf28f0700a
lee14k <kmkovach@mtu.edu>
2025-03-16 12:18:44 -0500

    fixing event linking

--- Components/Eventsgrid.jsx
+++ Components/Eventsgrid.jsx
@@ -12,49 +12,50 @@ export default function Eventsgrid({ events }) {
     setFlipStates(newFlipStates);
   };
 
+  const handleLinkClick = (e, link) => {
+    e.stopPropagation();
+    window.open(link, '_blank', 'noopener,noreferrer');
+  };
+
   return (
-    <div className="gridwrapper front ">
+    <div className="gridwrapper front">
       {events.map((event, index) => (
         <div
           onClick={() => handleFlip(index)}
-          className={`griditem ${flipStates[index] ? "flip" : ""} `}
+          className={`griditem ${flipStates[index] ? "flip" : ""}`}
           key={index}
           style={{
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
         >
-          <h2 className="gridhead font-bold ">
+          <h2 className="gridhead font-bold">
             <Eventspageevent
               header={event.title.rendered}
               date={event.date}
               time={event.time}
               price={event.price.rendered}
             />
-           
           </h2>
 
-          <div className="back " onClick={() => handleFlip(index)}>
-            
+          <div className="back">
             <Eventspageevent
               header={event.title.rendered}
               description={event.description}
               price={event.price.rendered}
-
-              
             />
-             {event.acf && event.acf.link && (
-              <a
-                href={event.acf.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="event-link-button"
+            {event.acf && event.acf.link && (
+              <div 
+                className="mt-4 z-10 relative" 
                 onClick={(e) => e.stopPropagation()}
               >
-                <button className="block w-full rounded-md bg-red-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">
+                <button 
+                  className="block w-full rounded-md bg-red-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+                  onClick={(e) => handleLinkClick(e, event.acf.link)}
+                >
                   More Info
                 </button>
-              </a>
+              </div>
             )}
           </div>
         </div>
